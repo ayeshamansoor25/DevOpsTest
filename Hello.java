90b1141d9c44a0c062c4da8c52f1091a6079cbbc
Ayesha Mansoor <ayeshamansoor2005@gmail.com>
2026-09-24 18:53:35 +0500

    removed extra testing lines

--- Hello.java
+++ Hello.java
@@ -2,7 +2,5 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("Hello from Jenkins!");
         System.out.println("Hello from Jenkins again!");
-        System.out.println("i'm hungry i just wanna go T.T");
-        System.out.println("new webhook test 123");
     }
 }
